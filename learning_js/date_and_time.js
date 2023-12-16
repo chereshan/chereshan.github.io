4315176d8bec6a785e9b7f446e97a4333c2b78e1
chereshan <55876010+chereshan@users.noreply.github.com>
2023-12-16 11:52:01 +0700

    add new js code examples

--- learning_js/date_and_time.js
+++
@@ -1,6 +0,0 @@
-let timestamp = Date.now(); //Текущее время как отметка времени (число) .
-let now = new Date(); // Текущее время как объект Date.
-let ms = now.getTime(); // Преобразовать в миллисекундную
-let iso = now.toISOString(); // Преобразовать в строку со стандартным
-console.log(timestamp,
-    now, ms, iso)
---
+++ scripts/header.js
@@ -0,0 +1,1 @@
+$(function(){$("#header").load("../common/header.html");});
