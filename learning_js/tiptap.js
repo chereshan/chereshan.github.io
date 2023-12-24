8f9732c0444180ea674012192c5acd9c8a291bd1
chereshan <55876010+chereshan@users.noreply.github.com>
2023-12-24 07:23:19 +0700

    html substring code refactoring

---
+++ learning_js/tiptap.js
@@ -0,0 +1,10 @@
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+
+new Editor({
+    element: document.querySelector('.element'),
+    extensions: [
+        StarterKit,
+    ],
+    content: '<p>Hello World!</p>',
+})
