df55437f7850c1269f32f453458cd9f1da93fcc8
chereshan <55876010+chereshan@users.noreply.github.com>
2024-02-27 05:20:43 +0700

    add autoindex for textbook

--- scripts/body_scripts.js
+++ scripts/body_scripts.js
@@ -4,3 +4,15 @@ $(document).ready(function(){
         $(this).html('<span class="code">' + $(this).html() + '</span>')
     })
 })
+
+jQuery('h1').after('<ul id="autonav"><ul>')
+h=[0,0,0,0,0];pos_memory=0;
+jQuery(':header:not(h1)').each(function () {
+    pos=+($(this)[0].nodeName.slice(-1))-2
+    h[pos]+=1
+    if (pos_memory>pos) h[pos_memory]=0
+    pos_memory=pos
+    $(this).attr('id','ch-'+(h.filter((n)=>n!=0).join('.')));
+    jQuery('#autonav').append(`<li><a href=${'#'+$(this).attr('id')}>${$(this).attr('id').slice(3)}. ${$(this).text()}</a></li>`);
+    $(this).html($(this).attr('id').slice(3)+'. '+$(this).html())
+})
